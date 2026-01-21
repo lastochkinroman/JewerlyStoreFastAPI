d55e9fb1ded65a6e252e2658230b709b40d44619
Lastochkin <rdlastochkin@gmail.com>
2026-01-21 07:08:02 +0400

    feat: Реализована логика CRUD для товаров и подключены эндпоинты

--- app/main.py
+++ app/main.py
@@ -6,6 +6,18 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Jewerly CRM")
 
-@app.get("/")
-def read_root():
-    return {"message": "CRM система ювелирного магазина готова к работе"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post('/items', response_model=schemas.Item)
+def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    return crud.create_item(db=db, item=item)
+
+@app.get('/items/', response_model = list[schemas.Item])
+def read_items(skip: int=0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
