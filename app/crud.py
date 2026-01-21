e3c328912aea75a1c02ebf41a16ce67f7530587a
Lastochkin <rdlastochkin@gmail.com>
2026-01-21 07:26:15 +0400

    feat: Добавлен функционал добавления клиентов с простой проверкой на уникальность телефона

--- app/crud.py
+++ app/crud.py
@@ -13,3 +13,19 @@ def get_items(db: Session, skip: int = 0, limit: int = 100):
 
 def get_item_by_id(db: Session, item_id: int):
     return db.query(models.Item).filter(models.Item.id == item_id).first()
+
+def get_customer_by_phone(db: Session, phone: str):
+    return db.query(models.Customer).filter(models.Customer.phone == phone).first()
+
+def create_customer(db: Session, customer: schemas.CustomerCreate):
+    db_customer = models.Customer(**customer.model_dump())
+    db.add(db_customer)
+    db.commit()
+    db.refresh(db_customer)
+    return db_customer
+
+def get_customers(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Customer).offset(skip).limit(limit).all()
+
+def get_item_by_article(db:Session, article: str):
+    return db.query(models.Item).filter(models.Item.article == article).first()
--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
-from . import models
-from .database import engine
+from fastapi import Depends, FastAPI, HTTPException
+from .database import SessionLocal, engine
+from . import schemas, models
+from sqlalchemy.orm import Session
+from . import crud
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -13,11 +15,25 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/items', response_model=schemas.Item)
+@app.post('/items/', response_model=schemas.Item)
 def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    db_item = crud.get_item_by_article(db, article=item.article)
+    if db_item:
+        raise HTTPException(status_code=400, detail="Артикул уже существует")
     return crud.create_item(db=db, item=item)
 
 @app.get('/items/', response_model = list[schemas.Item])
 def read_items(skip: int=0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
+
+@app.get("/customers/", response_model=list[schemas.Customer])
+def read_customers(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.get_customers(db, skip=skip, limit=limit)
+
+@app.post("/customers", response_model=schemas.Customer)
+def create_customer(customer: schemas.CustomerCreate, db: Session = Depends(get_db)):
+    db_customer = crud.get_customer_by_phone(db, phone = customer.phone)
+    if db_customer:
+        raise HTTPException(status_code=400, detail='Клиент с таким телефоном уже зарегистрирован')
+    return crud.create_customer(db=db, customer=customer)
